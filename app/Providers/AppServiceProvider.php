995eb878889efbeca749506a78eb6302e86033c4
keochamraeun54@gmail.com <keochamraeun54@gmail.com>
2025-07-24 15:15:00 +0700

    dd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,22 +7,15 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
-
-        class_alias(Carbon::class, 'MyCarbon');
-
+        if (!class_exists('MyCarbon')) {
+            class_alias(Carbon::class, 'MyCarbon');
+        }
     }
 }
