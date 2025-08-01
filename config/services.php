8940729447d61869441ac257d4257cf95b02f061
keochamraeun54@gmail.com <keochamraeun54@gmail.com>
2025-08-01 09:08:09 +0700

    dd

--- config/services.php
+++ config/services.php
@@ -6,18 +6,11 @@ return [
     |--------------------------------------------------------------------------
     | Third Party Services
     |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
     */
 
     'zoom' => [
         'timezone' => config('app.timezone'),
     ],
-
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
@@ -34,6 +27,11 @@ return [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    // Add this Google section:
+    'google' => [
+        'cloud_api_key' => env('GOOGLE_CLOUD_API_KEY'),
     ],
 
 ];
